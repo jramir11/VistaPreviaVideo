065bbee22e6cabfc06c2487780f6ff3ead05c0fd
Javier Ramirez <jramir11@bancoestado.cl>
2022-10-18 10:49:12 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,14 @@
+
+var vid = document.getElementById("VideoMP4-tag"); 
+
+function playVid() { 
+      vid.muted = true;
+      vid.play(); 
+}
+function pauseVid() { 
+    vid.pause(); 
+} 
+function mutedVid() {
+    vid.muted = true;
+}
+
